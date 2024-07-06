74c42f9bf99ee0fd041bc592ee2cd17bea61bdf9
aigerimkarataeva <ai.gerim@hotmail.com>
2024-07-06 20:06:44 +0600

    styled errors on registration page

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -9,6 +9,7 @@ import lorby from '../../styles/svg/lorby.svg';
 import style from './Register.module.scss';
 import { ReactComponent as OpenEye } from '../../styles/svg/open-eye.svg';
 import { ReactComponent as ShutEye } from '../../styles/svg/shut-eye.svg';
+import { ReactComponent as ArrowIcon } from '../../styles/svg/arrow.svg';
 
 const Register = () => {
   const dispatch = useDispatch();
@@ -29,7 +30,9 @@ const Register = () => {
   return (
     <div className={style.register_page}>
       <div className={style.back_button}>
-        <Link to="/login">Back</Link>
+        <Link to="/login">
+          <ArrowIcon width="16" height="16" /> Back
+        </Link>
       </div>
       <div className={style.content}>
         <div className={style.left}>
@@ -48,43 +51,42 @@ const Register = () => {
               validationSchema={registrationSchema}
               onSubmit={handleSubmit}
           >
-              {({ isSubmitting, values }) => (
+              {({ isSubmitting, errors, touched, values }) => (
               <Form>
                   <div>
-                  <Field type="email" name="email" placeholder='Enter your email'  className={style.field}/>
-                  <ErrorMessage name="email" component="div" className={style.error} />
+                    <Field 
+                    type="email" 
+                    name="email" 
+                    placeholder='Enter your email'
+                    className={`${style.field} ${touched.email && errors.email ? style.error : ''}`}
+                    />
                   </div>
                   <div>
-                  <Field type="text" name="username" placeholder='Choose username' className={style.field}/>
-                  <ErrorMessage name="username" component="div" className={style.error} />
+                    <Field 
+                    type="text" 
+                    name="username" 
+                    placeholder='Choose username' 
+                    className={`${style.field} ${touched.username && errors.username ? style.error : ''}`}
+                    />
                   </div>
-                  <div>
-                  <Field type={passwordVisible ? "text" : "password"} name="password" placeholder='Create a password' className={style.field}/>
-                  <span onClick={() => setPasswordVisible(!passwordVisible)}>
-                      {passwordVisible ? (
-                        <ShutEye width="24" height="24" />
-                      ) : (
-                        <OpenEye width="24" height="24" />
-                      )}
-                    </span>
-                  <ErrorMessage name="password" component="div" className={style.error}/>
+                  <div className={style.passwordField}> 
+                    <div className={style.passwordInput}>
+                      <Field
+                      type={passwordVisible ? "text" : "password"} 
+                      name="password" 
+                      placeholder='Create a password'
+                      className={`${style.field} ${touched.password && errors.password ? style.error : ''}`}
+                      />
+                      <span onClick={() => setPasswordVisible(!passwordVisible)}>
+                          {passwordVisible ? (
+                            <ShutEye width="24" height="24" />
+                          ) : (
+                            <OpenEye width="24" height="24" />
+                          )}
+                        </span>
+                    </div>
                   </div>
-                  <div>
-                  
-                  <Field type={passwordVisible ? "text" : "password"} name="confirmPassword" placeholder='Confirm your password' className={style.field}/>
-                  <ErrorMessage name="confirmPassword" component="div" className={style.error}/>
-                  </div>
-                  <div>
-                  <button type="button" onClick={() => setPasswordVisible(!passwordVisible)}>
-                      {passwordVisible ? 'Hide' : 'Show'} Password
-                  </button>
-                  </div>
-                  <div>
-                  <button type="submit" disabled={isSubmitting} className={style.sbm_btn}>
-                      Register
-                  </button>
-                  </div>
-                  <div>
+                  <div className={style.password_errors}>
                   {values.password && (
                       <ul>
                       <li style={{ color: /[A-Z]/.test(values.password) ? 'green' : 'red' }}>
@@ -104,6 +106,29 @@ const Register = () => {
                       </li>
                       </ul>
                   )}
+                  </div>
+                  <div className={style.passwordField}>
+                    <div className={style.passwordInput}>
+                      <Field 
+                      type={passwordVisible ? "text" : "password"} 
+                      name="confirmPassword" 
+                      placeholder='Confirm your password' 
+                      className={`${style.field} ${touched.confirmPassword && errors.confirmPassword ? style.error : ''}`}
+                      />
+                      <span onClick={() => setPasswordVisible(!passwordVisible)}>
+                        {passwordVisible ? (
+                          <ShutEye width="24" height="24" />
+                        ) : (
+                          <OpenEye width="24" height="24" />
+                        )}
+                        </span>
+                    </div>
+                    <ErrorMessage name="confirmPassword" component="div" className={style.password_match}/>
+                  </div>
+                  <div>
+                    <button type="submit" disabled={isSubmitting} className={style.sbm_btn}>
+                        Register
+                    </button>
                   </div>
               </Form>
               )}
