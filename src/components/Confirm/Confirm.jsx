2cfe7363c0efd4a09396fac370e3c2f8073f9b4b
aigerimkarataeva <ai.gerim@hotmail.com>
2024-07-11 21:29:52 +0600

    styled the confirmation page

--- src/components/Confirm/Confirm.jsx
+++ src/components/Confirm/Confirm.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { confirmEmail, resendConfirmationCode } from '../../features/auth/authSlice';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import style from './Confirm.module.scss';
+import lorby from '../../styles/svg/lorby.svg';
+import { ReactComponent as ArrowIcon } from '../../styles/svg/arrow.svg';
 
 const Confirm = () => {
   const [code, setCode] = useState('');
@@ -25,24 +28,49 @@ const Confirm = () => {
   };
 
   return (
-    <div>
-      <p>
-        We have sent a confirmation letter to your email. Please check and confirm. If you haven't received it, please check your spam folder.
-      </p>
-      <form onSubmit={handleConfirmEmail}>
-        <input
-          type="text"
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-          placeholder="Enter confirmation code"
-          required
-        />
-        <button type="submit">Confirm Email</button>
-      </form>
-      <button onClick={handleResendEmail}>Resend Confirmation Email</button>
-      {confirmationStatus === 'loading' && <p>Loading...</p>}
-      {confirmationStatus === 'failed' && <p>Error: {confirmationError}</p>}
-      {confirmationStatus === 'succeeded' && <p>Email confirmed successfully!</p>}
+    <div className={style.confirm_page}>
+        <div className={style.back_button}>
+            <Link to="/register">
+                <ArrowIcon width="16" height="16" /> Back
+            </Link>
+        </div>
+        <div className={style.content}>
+            <div className={style.left}>
+                <div className={style.register_img}>
+                    <img src={lorby} alt="lorby" />
+                </div>
+                <div className={style.heading}>
+                    <h1>Lorby</h1>
+                    <p>Your personal tutor</p>
+                </div>
+            </div>
+            <div className={style.right}>
+                <div className={style.section}>
+                    <p className={style.confirmEmail}>
+                        We have sent a confirmation letter with a code to your email. Please check and enter the code down bellow.
+                    </p>
+                    <p className={style.confirmError}> If you haven't received it, do not panic - please check your <strong>spam folder</strong>.</p>
+                    <p className={style.emoji}>(´｡• ω •｡`)</p>
+                    <form onSubmit={handleConfirmEmail}>
+                        <input
+                        type="text"
+                        value={code}
+                        onChange={(e) => setCode(e.target.value)}
+                        placeholder="Enter confirmation code"
+                        required />
+                        <button type="submit">Confirm Email</button>
+                    </form>
+                    <div>
+                        <button onClick={handleResendEmail}>I didn't get the email</button>
+                    </div>
+                    
+                    {confirmationStatus === 'loading' && <p>Loading...</p>}
+                    {confirmationStatus === 'failed' && <p>Error: {confirmationError}</p>}
+                    {confirmationStatus === 'succeeded' && <p>Email confirmed successfully!</p>}
+                </div>
+            </div>
+                
+      </div>
     </div>
   );
 };
